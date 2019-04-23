2dfd5f5e86085352e1c3ad42c73cc9309866e27c
Jim-Garner <47324596+Jim-Garner@users.noreply.github.com>
2019-04-24 00:05:52 +0100

    Upload

---
+++ Semester Two/Lab1 - Resizeable Arrays/Lab1 - Resizeable Arrays/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Lab1___Resizeable_Arrays
+{
+    class ResizeableArray
+    {
+        //Create a blank array;
+        public int[] Array = new int[0];
+
+
+        //Add a method that will add ints to array;
+        public void AddInt(int numberToAdd)
+        {
+            //Create a new array, making room for the new number;
+            int[] newArray = new int[Array.Length + 1];
+
+
+            //Loop through old array and copy into new array;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                //New array index is assigned Array index value, copying it over.
+                newArray[i] = Array[i];
+            }
+
+            //This will add the numberToAdd to the VERY end of the array -1 index position, or it would be out of bounds.
+            newArray[newArray.Length - 1] = numberToAdd;
+
+            //NewArray now becomes current array;
+            Array = newArray;
+        }
+
+
+        public void printArray()
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine(Array[i]);
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling Class, new object my Array = new array;
+            ResizeableArray myArray = new ResizeableArray();
+
+            //Method AddInt to myArray
+            myArray.AddInt(5);
+            myArray.AddInt(12);
+
+
+            //Print my Array
+            myArray.printArray();
+        }
+    }
+
+
+}
---
+++ Semester Two/Lab1.2 - Generics/Lab1.2 - Generics/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1._2___Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lists
+
+            //Declare the list name and type;
+            List<string> names = new List<string>();
+
+            names.Add("James");
+            names.Add("Timothy");
+            names.Add("Joe");
+            names.Add("Dawn");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            //Dictionarys
+            //Here we declare a dictionary called garage;
+            Dictionary<string, int> garage = new Dictionary<string, int>();
+
+            //Dictionary's need a key and a value, like an ordinary one needs a word and a definition.
+
+            garage.Add("Ferrari Sport", 2);
+            garage.Add("Bugatti Vehron", 1);
+            garage.Add("Aston Martin DBS", 2);
+
+            if (garage.ContainsKey("Ferrari Sport"))
+            {
+                int value = garage["Ferrari Sport"];
+                Console.WriteLine(value);
+            }
+
+            if (garage.ContainsKey("Bugatti Vehron"))
+            {
+                Console.WriteLine("True.");
+            }
+            else
+            {
+                Console.WriteLine("False.");
+            }
+        }
+    }
+}
